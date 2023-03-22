95edade98dbe9122fdff1c0336785877a1e85a4e
akramcheraifia <hebdu636dheb@gmail.com>
2023-03-22 13:43:59 +0100

    user resource added

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Page;
+use Filament\Resources\Form;
+use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->maxLength(255)
+                            ->autofocus()
+                            ->placeholder('Enter a name...'),
+                        TextInput::make('email')
+                            ->required()
+                            ->placeholder('Enter an email...')
+                            ->maxLength(255)
+                            ->email(),
+                        TextInput::make('password')
+                            ->required(fn (Page $livewire): bool => $livewire instanceof CreateRecord)
+                            ->placeholder('Enter a password...')
+                            ->minLength(8)
+                            ->password()
+                            ->same('passwordConfirmation')
+                            ->dehydrated(fn ($state) => filled($state))
+                            ->dehydrateStateUsing(fn ($state)=> Hash::make($state)),
+                            TextInput::make('passwordConfirmation')
+                            ->required(fn (Page $livewire): bool => $livewire instanceof CreateRecord)
+                            ->placeholder('Confirm the password...')
+                            ->minLength(8)
+                            ->same('password')
+                            ->dehydrated(false)
+                            ->password(),
+
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('email')->searchable()->sortable(),
+                TextColumn::make('created_at')->dateTime()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
