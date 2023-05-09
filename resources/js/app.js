fb24c8e2f86caa8b45e9c348e3cf3c4a34980caf
akramcheraifia <hebdu636dheb@gmail.com>
2023-05-09 23:17:16 +0200

    bug fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import './vendor/alperenersoy/filament-export/resources/js/filament-export.js';
+import '../../vendor/alperenersoy/filament-export/resources/js/filament-export.js';
 
 
 window.Alpine = Alpine;
